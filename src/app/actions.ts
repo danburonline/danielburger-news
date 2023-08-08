eebf7563993c9e13b9b4af3ff53273d2f3f735ad
Daniel Burger <public@danielburger.online>
2023-08-08 19:18:58 +0200

    `var` to `let`

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,14 +10,14 @@ function isEmailValid(email: string) {
 
   if (email.length > 254) return false
 
-  var valid = emailRegex.test(email)
+  let valid = emailRegex.test(email)
   if (!valid) return false
 
   // Further checking of some things regex can't handle
-  var parts = email.split('@')
+  let parts = email.split('@')
   if (parts[0].length > 64) return false
 
-  var domainParts = parts[1].split('.')
+  let domainParts = parts[1].split('.')
   if (
     domainParts.some(function (part) {
       return part.length > 63
