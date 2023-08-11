9c04a4ccd86565f77b3a30b88b1383c48bd404dd
Daniel Burger <public@danielburger.online>
2023-08-11 12:49:15 +0200

    Using `<a />` instead of `<Link />`

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,12 +15,12 @@ export default function NotFound() {
       <body>
         <main className="h-[100dvh] min-h-[300px] w-screen flex flex-col justify-center items-center p-6">
           <h1 className="text-white font-bold text-3xl text-center px-8 mb-6">Page Not Found</h1>
-          <Link
+          <a
             href="/"
             className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-3 text-sm font-semibold text-black shadow-sm hover:bg-primaryDark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primaryLight mt-0 w-auto"
           >
             Go to start page
-          </Link>
+          </a>
         </main>
       </body>
     </>
