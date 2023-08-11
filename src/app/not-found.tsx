625010cec390c6127caac824627c0b936715380e
Daniel Burger <public@danielburger.online>
2023-08-11 12:33:03 +0200

    Still have a proper 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,24 @@
 'use client'
 
-import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
   const router = useRouter()
 
-  useEffect(() => {
-    // Always redirect to the main page if the user lands on a 404
+  const handleGoHome = () => {
     router.push('/')
-  })
+  }
+
+  return (
+    <main className="h-[100dvh] min-h-[300px] w-screen flex flex-col justify-center items-center p-6">
+      <h1 className="text-white font-bold text-3xl text-center px-8 mb-6">Page Not Found</h1>
+      <button
+        type="submit"
+        className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-3 text-sm font-semibold text-black shadow-sm hover:bg-primaryDark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primaryLight mt-0 w-auto"
+        onClick={handleGoHome}
+      >
+        Go to start page
+      </button>
+    </main>
+  )
 }
